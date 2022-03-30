0a37a992e617631e3a12570cf1969e185ec394d6
hashsploit <hashsploit@protonmail.com>
2022-03-30 12:15:47 -0700

    Removed unnecessary dependency check

--- docker-build.sh
+++ docker-build.sh
@@ -4,11 +4,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-# Check if maven is installed
-if ! command -v mvn &> /dev/null; then
-    echo -e "$(tput bold)$(tput setaf 1)Maven is not installed, please install maven!$(tput sgr0)"
-    exit 1
-fi
-
 docker build -t img-ocr-validator .
 
